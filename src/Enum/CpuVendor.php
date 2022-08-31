2fd902fdc31944d29d976b3b87b3cd549b25c0bf
tienvx <tien.xuan.vo@gmail.com>
2022-08-31 22:39:45 +0700

    Index cpu specs

--- src/Enum/CpuVendor.php
+++ src/Enum/CpuVendor.php
@@ -6,4 +6,12 @@ enum CpuVendor: string
 {
     case AMD = 'AMD';
     case INTEL = 'Intel';
+
+    public static function values(): array
+    {
+        return array_map(
+            fn (CpuVendor $vendor) => $vendor->value,
+            CpuVendor::cases()
+        );
+    }
 }
